8d3ed6d3c811c2b425531a8a84957eb9361113b0
Adekabbey <adekabbey@gmail.com>
2022-09-12 16:17:30 -0700

    My Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,16 +8,14 @@
  *
  * the string is printed with the printf function
  *
-* Return: (0) Success
- *
- *      */
+ * Return: (0) Success
+ */
 
 int main(void)
 
 {
-
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 
-			return (0);
+		return (0);
 
 }
