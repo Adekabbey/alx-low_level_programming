ef8fffe7a904bb53780eebad2d2a8bf587718de1
Adekabbey <adekabbey@gmail.com>
2022-09-12 16:01:00 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main-entry point
+ *
+ * The string is printing with the printf function
+ *
+ * Return: (0) success
+ *
+ * int main(void)
+ *
+ * {
+ *
+ * 	printf('with proper grammar, but the outcome is a piece of art,'
+ *
+ * 	return (0) 
+ *
+ * 	}
