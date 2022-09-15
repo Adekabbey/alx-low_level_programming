9f0e22954b26f44a0bbb8eeb0a399fbf99282252
Adekabbey <adekabbey@gmail.com>
2022-09-14 22:48:40 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 success
+ */
+
+int main()
+	{
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+		return (0);
+	}
